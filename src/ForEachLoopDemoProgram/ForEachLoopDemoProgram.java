b3365fc7a2d24066d2c7994e39ac87c800025868
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-11-09 21:04:24 +0600

    for each loop demo program

---
+++ src/ForEachLoopDemoProgram/ForEachLoopDemoProgram.java
@@ -0,0 +1,44 @@
+
+package ForEachLoopDemoProgram;
+
+/**
+ *
+ * @author Nayeem
+ */
+public class ForEachLoopDemoProgram {
+    public static void main(String[]args)
+    {
+        /**
+//variable diclarartion area
+        int i;
+        
+    //aikhane array  length holo 4 ti orthat aikhane  string type  names variable er modhe 4ta string type name declare  kora hoyese.     
+    String[] names={"Nayeem","Ridoy","Bulbul","Rony"};
+    for( i=0;i<names.length;i++)
+    {
+    
+    System.out.println(names[i]);
+    
+    
+    
+    }
+    * */
+    
+       
+    //for each loop demo program    
+   String[]Customernames={"Nayeem","Apu","Raju","Tohid"};
+   
+   for(String temp:Customernames)
+   {
+   
+   System.out.println(temp);
+   }
+   
+           
+    
+    
+    
+    
+    }
+    
+}
